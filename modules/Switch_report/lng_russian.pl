bfada8e1e811f443010644b1109b076e88d2c7a1
YuriyK13 <102144257+YuriyK13@users.noreply.github.com>
2022-05-18 16:41:34 +0300

    Update lng_russian.pl

--- modules/Switch_report/lng_russian.pl
+++ modules/Switch_report/lng_russian.pl
@@ -1,5 +1,6 @@
-$lang{SWITCH} = 'Комутатор';
-$lang{USERS} = 'Клиенты';
+$lang{TITLE_SWITCH_REPORT} = 'Отчет по коммутаторам';
+$lang{SWITCH} = 'Коммутаторы';
+$lang{USERS} = 'Пользователи';
 $lang{USERS_OFF} = 'Отключенные';
 $lang{USERS_ON} = 'Включенные';
 $lang{QUANTITY_USERS_REQUEST} = 'Заявки за посл 30 дн.';
