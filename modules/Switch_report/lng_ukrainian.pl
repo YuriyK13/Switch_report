7ed57d16c1e4880799bd9a50d0a34bb6105feada
YuriyK13 <102144257+YuriyK13@users.noreply.github.com>
2022-05-18 16:41:00 +0300

    Update lng_ukrainian.pl

--- modules/Switch_report/lng_ukrainian.pl
+++ modules/Switch_report/lng_ukrainian.pl
@@ -1,7 +1,8 @@
-$lang{SWITCH} = 'Комутатор';
-$lang{USERS} = 'Клієнти';
-$lang{USERS_OFF} = 'Відключених';
-$lang{USERS_ON} = 'Включених';
+$lang{TITLE_SWITCH_REPORT} = 'Звіт по комутаторам';
+$lang{SWITCH} = 'Комутатори';
+$lang{USERS} = 'Користувачі';
+$lang{USERS_OFF} = 'Відключені';
+$lang{USERS_ON} = 'Включені';
 $lang{QUANTITY_USERS_REQUEST} = 'Заявки за ост 30 дн.';
 $lang{COEFFICIENT_OF_DISABLE_USERS} = 'Коефіцієнт відключених';
 $lang{COEFFICIENT_OF_REQUESTS_USERS} = 'Коефіцієнт заявок';
